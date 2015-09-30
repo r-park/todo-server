4a21ef1b300b6777c9878b909377a714943b3a74
Richard Park <objectiv@gmail.com>
2015-09-29 21:54:00 -0400

    chore(mongodb): change uri

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 
 var config = {
   development: {
-    uri: 'mongodb://127.0.0.1:27017/todo-api',
+    uri: 'mongodb://127.0.0.1:27017/tasks',
     options: {
       user: '',
       pass: ''
@@ -10,7 +10,7 @@ var config = {
   },
 
   test: {
-    uri: 'mongodb://127.0.0.1:27017/todo-api-test',
+    uri: 'mongodb://127.0.0.1:27017/tasks-test',
     options: {
       user: '',
       pass: ''
